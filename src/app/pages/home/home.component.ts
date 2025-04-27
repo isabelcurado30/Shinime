016b3471f39e209511834b0d8b2b14effedac6e4
Isabel Curado España <marisabelcurado.es@gmail.com>
2025-04-28 00:08:39 +0200

    página de inicio hecha (por ahora)

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component ({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent implements OnInit{
+
+  trendingAnimes: any[] = [];
+  newReleases: any[] = [];
+  recommendedAnimes: any[] = [];
+  topAnimes: any[] = [];
+
+  constructor (private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadTrending();
+    this.loadNewReleases();
+    this.loadRecommendations();
+    this.loadTop(); 
+  }
+
+  loadTrending() {
+    this.http.get <any> ('https://api.jikan.moe/v4/top/anime').subscribe (response => {
+      this.trendingAnimes = response.data.slice (0, 8);
+    });
+  }
+
+  loadNewReleases() {
+    this.http.get <any> ('https://api.jikan.moe/v4/seasons/now').subscribe (response => {
+      this.newReleases = response.data.slice (0, 8);
+    });
+  }
+
+  loadRecommendations() {
+    this.http.get <any> ('https://api.jikan.moe/v4/recommendations/anime').subscribe (response => {
+      this.recommendedAnimes = response.data.slice (0, 8).map ((rec: any) => rec.entry [0]);
+    });
+  }
+
+  loadTop() {
+    this.http.get <any> ('https://api.jikan.moe/v4/top/anime').subscribe (response => {
+      console.log ('Top Animes:', response.data);
+      this.topAnimes = response.data.slice (0, 5);
+    });
+  }
+}
