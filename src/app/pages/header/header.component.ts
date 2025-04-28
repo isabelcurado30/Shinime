cdc590575d85161dba3fd1fe368894e9a49d6e92
Isabel Curado España <marisabelcurado.es@gmail.com>
2025-04-28 11:20:45 +0200

    header hecho (por ahora)

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({
+@Component ({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent {
 
+  isLoggedIn = false;
+  isMobileMenuOpen = false;
+
+  constructor (private router: Router) {}
+
+  navigateToHome() {
+    this.router.navigate (['/']);
+  }
+
+  navigateToLogin() {
+    this.router.navigate (['/login']);
+  }
+
+  navigateToRegister() {
+    this.router.navigate (['/register']);
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.router.navigate (['/']);
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
 }
